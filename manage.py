a1f2f1ce89659750804daf2897a793c6388a18f3
MayankNinaniya725 <mninaniya499@gmail.com>
2025-08-28 17:26:53 +0530

    Tested and made changes accordingly

--- manage.py
+++ manage.py
@@ -29,9 +29,16 @@ if __name__ == '__main__':
 # docker compose up -d for starting the services in detached mode
 # docker compose down for stopping the services
 # docker compose logs -f for viewing the logs
-# docker compose up --build -d for rebuilding the images and starting the services in detached mode     
+# docker compose up --build  for rebuilding the images and starting the services when debugging, so you can see real-time logs in the terminal. 
+# docker compose up --build -d for rebuilding the images and starting the services in detached mode or for normal background running (production/dev).
 # docker compose exec web python manage.py migrate for applying migrations
 # docker compose exec web python manage.py createsuperuser for creating a superuser
+# PS C:\Users\Mayank\desktop\dee\extractor_project> docker compose exec web python manage.py flush 
+
+# docker compose exec web python manage.py shell for removing files for testing the files again and again
+# from extractor.models import UploadedPDF, ExtractedData
+# UploadedPDF.objects.all().delete()
+# ExtractedData.objects.all().delete()
 
 #  http://localhost:8000/admin
 #  http://localhost:8000/upload/ for uploading PDFs
